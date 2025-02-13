eef6b9361279412623b8cffecf70a8122c8576dc
indhiraPriyadarsini <iipriyadarsini26@gmail.com>
2025-02-13 21:13:54 +0530

    Added day 3 assignments

--- Day-2/provider.tf
+++ Day-2/provider.tf
@@ -1,5 +1,5 @@
 terraform {
-  backend "s3" {
+  backend "s3" {  //Day 3 - state backend with s3 and dynamodb
     bucket = "ipd-s3-state-bucket"
     key    = "terraform-state"
     region = "us-east-1"
